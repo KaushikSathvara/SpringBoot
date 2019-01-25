65f2bac31251947a75367b794ac77c28a8478a28
kaushik <kaushiksathvara74@gmail.com>
2019-01-25 18:17:58 +0530

    Accepting Client Data with HttpServlet request and response

--- SpringBootSample/src/main/java/com/SpringBootDemo/MainControllerClass.java
+++ SpringBootSample/src/main/java/com/SpringBootDemo/MainControllerClass.java
@@ -1,13 +1,25 @@
 package com.SpringBootDemo;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 public class MainControllerClass {
 	@RequestMapping("home")
-	public String hello(){
-		System.out.println("hello");
+	public String hello(HttpServletRequest req,HttpServletResponse res){
+		HttpSession session=req.getSession();
+		String name=req.getParameter("name");
+		session.setAttribute("name",name);
+//		 System.out.println(req.getParameter("name"));
+//		System.out.println("hi"+name);
 		return "index";
+	
 	}
 }
--- SpringBootSample/src/main/webapp/view/index.jsp
+++ SpringBootSample/src/main/webapp/view/index.jsp
@@ -7,7 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-This is
+This is ${name}
 
 </body>
 </html>

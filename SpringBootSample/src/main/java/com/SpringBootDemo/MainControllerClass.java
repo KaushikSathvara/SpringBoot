701191ff463f2e981964b15c387e06cd502cb471
kaushik <kaushiksathvara74@gmail.com>
2019-01-25 18:31:46 +0530

    ModelAndView Concept Implemented

--- SpringBootSample/src/main/java/com/SpringBootDemo/MainControllerClass.java
+++ SpringBootSample/src/main/java/com/SpringBootDemo/MainControllerClass.java
@@ -7,19 +7,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
 @Controller
 public class MainControllerClass {
 	@RequestMapping("home")
-	public String hello(HttpServletRequest req,HttpServletResponse res){
-		HttpSession session=req.getSession();
-		String name=req.getParameter("name");
-		session.setAttribute("name",name);
-//		 System.out.println(req.getParameter("name"));
-//		System.out.println("hi"+name);
-		return "index";
+	public ModelAndView hello(@RequestParam("name")String name){
+		ModelAndView mv=new ModelAndView();
+		
+		mv.addObject("name",name);
+		mv.setViewName("index");
+		
+		return mv;
 	
 	}
 }
